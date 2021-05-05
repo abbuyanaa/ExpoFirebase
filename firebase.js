d97b37e062ec6a0cd627ea285d89d07b786f8784
abbuyanaa <abbuyanaa@gmail.com>
2021-05-05 22:31:03 +0800

    Added firebase.js file

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase";
+
+import "firebase/firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAc9rTn3cOdm1ueZt_xX8JcszyEh3fTLL4",
+  authDomain: "expofirebase-5504a.firebaseapp.com",
+  databaseURL: "https://expofirebase-5504a-default-rtdb.firebaseio.com",
+  projectId: "expofirebase-5504a",
+  storageBucket: "expofirebase-5504a.appspot.com",
+  messagingSenderId: "218452678435",
+  appId: "1:218452678435:web:beaa51a93b21635d015e96",
+  measurementId: "G-ZGT3EGTLK4",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export default {
+  firebase,
+  db,
+};
